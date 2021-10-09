fd0a450e868ebc039efab4a85b3f59c0773ffc5e
alysonjan <alysonjan@gmail.com>
2021-10-09 20:41:42 +0800

    adding store in provider

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from "redux";
 import alert from "./alert";
-import auth from "./auth"
+import auth from "./auth";
+import subjectbudget from "./subjectbudget";
 
 export default combineReducers({
     alert,
-    auth
+    auth,
+    subjectbudget,
+
 });
