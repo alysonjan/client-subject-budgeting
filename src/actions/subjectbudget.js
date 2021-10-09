f40c34e8224a790792ce3998c0caa7fb4c937a34
alysonjan <alysonjan@gmail.com>
2021-10-09 20:49:50 +0800

    function for api post request from generate subject forms

---
+++ src/actions/subjectbudget.js
@@ -0,0 +1,25 @@
+import axiosInstance from "../helpers/axios";
+import { setAlert } from './alert';
+import { GENERATE_BUDGET,GENERATE_ERROR } from "../constants/constant";
+
+
+export const generateBudget = (formData,history) => async dispatch => {
+    try {
+        const res = await axiosInstance.post('/generate/subject/add',formData)
+        dispatch({
+            type:GENERATE_BUDGET,
+            payload: res.data
+        });
+        //@TODO success alert message
+        history.push('/subject-budget')
+
+    } catch (err) {
+        const errors = err.response.data.errors;
+        if (errors) errors.forEach(error => dispatch(setAlert(error.msg, 'danger' )));
+
+        dispatch({
+            type:GENERATE_ERROR,
+            payload: {msg: err.response.statusText, status:err.response.status}
+        });
+    }
+}
